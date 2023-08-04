db823c0d073ed96aa04f7a11efc3dc43ce68eb74
Gogs <gogs@fake.local>
2023-08-04 23:12:51 +0530

    added state for user

--- store/store.ts
+++ store/store.ts
@@ -2,25 +2,21 @@ import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
 
 type State = {
-  win1: string;
-  win2: string;
+  user: any;
 };
 
 type Action = {
-  updateWin1: (win1: State["win1"]) => void;
-  updateWin2: (win2: State["win2"]) => void;
+  updateUser: (user: State["user"]) => void;
 };
 
 const useStore = create(
   persist<State & Action>(
     (set) => ({
-      win1: "",
-      win2: "",
-      updateWin1: (win1) => set(() => ({ win1: win1 })),
-      updateWin2: (win2) => set(() => ({ win2: win2 })),
+      user: "vineeth",
+      updateUser: (user) => set(() => ({ user: user })),
     }),
     {
-      name: "windows",
+      name: "user",
     }
   )
 );
